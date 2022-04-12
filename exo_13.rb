62cbac33c268ae686ce6f33cd9bbe8d6e68fa0a0
Iris <marlx42@protonmail.com>
2022-04-12 16:06:55 +0200

    exo_13 + 14 added

---
+++ exo_13.rb
@@ -0,0 +1,7 @@
+mail = 01
+mail.upto(50) do |i|
+#names = Array.new(01, "jean.dupont.#{i}.email.fr")
+arr = "jean.dupont.#{i}@email.fr"
+#puts "#{names}"
+puts arr
+end
---
+++ exo_14.rb
@@ -0,0 +1,8 @@
+mail = 01
+mail.upto(50) do |i|
+arr = "jean.dupont.#{i}@email.fr"
+if i%2 == 0
+
+puts arr
+end
+end
