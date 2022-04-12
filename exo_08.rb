acf52727d6fcd78ff954d054ee071f085f836d13
Iris <marlx42@protonmail.com>
2022-04-12 10:20:56 +0200

    modification m to i + exo 9

--- exo_08.rb
+++ exo_08.rb
@@ -1,6 +1,6 @@
 puts "Give me a number"
 print "->"
 number = gets.chomp
-number.to_i.downto(0) do |m|
-  puts m
+number.to_i.downto(0) do |i|
+  puts i
 end
--- exo_09.rb
+++ exo_09.rb
@@ -1,4 +1,6 @@
 puts "What year were you born ?"
 print "->"
 year = gets.chomp
-year.to_i
+year.to_i.upto(2022) do |i|
+  puts i 
+end
